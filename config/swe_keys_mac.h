1d7fffe1b1ceafa5a621667326276905f1f93315
Viktor Ivarsson <vicke.i93@gmail.com>
2023-03-15 13:20:27 +0100

    yep

--- config/swe_keys_mac.h
+++ config/swe_keys_mac.h
@@ -91,8 +91,8 @@
 #define SEM_SQT   BSLH          // '
 #define SEM_GRAVE PLUS          // `
 #define SEM_TILDE LA(RBKT)      // ~
-#define SEM_LT    LS(COMMA)     // <
-#define SEM_GT    LS(DOT)       // >
+#define SEM_LT    NON_US_BSLH     // <
+#define SEM_GT    LS(NON_US_BSLH)       // >
 #define SEM_BSLH  LA(LS(N7))    // (backslash)
 #define SEM_PIPE  LA(N7)        // |
 
