1ef93842b04b04e79ed3a810b763c4b100b2f83b
Dyegho M C G Cunha <123116771+DyeghoCunha@users.noreply.github.com>
2024-11-25 14:59:47 -0300

    Update and rename keymap_italian.h to keymap_brazilian_abnt2.h

--- config/keymap_brazilian_abnt2.h
+++ config/keymap_brazilian_abnt2.h
@@ -1,75 +1,3 @@
-/**
- * This file includes custom keycodes for the italian input language setting.
- * If you only type in english, you can delete this file and it's import.
- * If you wish to replace this file with your preferred language, use
- * the qmk version as reference:
- * https://github.com/qmk/qmk_firmware/tree/master/quantum/keymap_extras
-*/
-
-
-/*
-
-// Row 1
-#define IT_BSLH GRAVE            // backslash
-#define IT_SQUOT MINUS           // '
-#define IT_IGRV EQUAL            // ì
-// Row 2
-#define IT_EGRV LBKT             // è
-#define IT_PLUS SLASH            // +
-// Row 3
-#define IT_OGRV SEMI             // ò
-#define IT_AGRV APOS             // à
-#define IT_UGRV NON_US_HASH      // ù
-// Row 4
-#define IT_LESS NON_US_BSLH      // <
-#define IT_MINUS RBKT            // -
-
-
-// Row 1
-#define IT_PIPE LS(GRAVE)        // |
-#define IT_EXCL LS(N1)           // !
-#define IT_DQUOT LS(N2)          // "
-#define IT_PND LS(N3)            // £
-#define IT_USD LS(N4)            // $
-#define IT_PERC LS(N5)           // %
-#define IT_AND LS(N6)            // &
-#define IT_FSLH LS(N7)           // /
-#define IT_LPAR LS(N8)           // (
-#define IT_RPAR LS(N9)           // )
-#define IT_EQL LS(N0)            // =
-#define IT_QUES LS(MINUS)        // ?
-#define IT_CARET LS(EQUAL)       // ^
-// Row 2
-#define IT_EACU LS(LBKT)         // é
-#define IT_AST LS(RBKT)          // *
-// Row 3
-#define IT_CCED LS(SEMI)         // ç
-#define IT_DEG LS(APOS)          // °
-#define IT_SECT LS(NON_US_HASH)  // §
-// Row 4
-#define IT_GREAT LS(NON_US_BSLH) // >
-#define IT_UNDER LS(SLASH)       // _
-
-
-// Row 2
-#define IT_EUR RA(E)             // €
-#define IT_LBKT RA(LBKT)         // [
-#define IT_RBKT RA(RBKT)         // ]
-// Row 3
-#define IT_AT RA(SEMI)           // @
-#define IT_HASH RA(APOS)         // #
-
-
-// Row 2
-#define IT_LBRC LS(RA(LBKT))     // {
-#define IT_RBRC LS(RA(RBKT))     // }
-
-
-#define IT_BKTK RA(MINUS)        // `
-#define IT_TILDE RA(EQUAL)       // ~
-#define IT_CEGR RA(LS(E))        // È
-
-
 // Copyright 2024 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -95,12 +23,11 @@
      "Y88P"
 *******************************************************************************/
 
-//#pragma once
-//#include "keymap_italian.h"
+#pragma once
+#include "keycodes.h"
 // clang-format off
 
 // Aliases
-
 #define BR_QUOT KC_GRV  // '
 #define BR_1    KC_1    // 1
 #define BR_2    KC_2    // 2
@@ -185,4 +112,3 @@
 #define BR_FORD ALGR(BR_LBRC) // ª
 #define BR_MORD ALGR(BR_RBRC) // º
 #define BR_CRUZ ALGR(BR_C)    // ₢
-
